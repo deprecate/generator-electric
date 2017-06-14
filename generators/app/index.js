f4505a6525dddea7abe6e50e5b7d401ad505f841
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-06-14 13:48:57 -0700

    gulpfile.js was removed

--- generators/app/index.js
+++ generators/app/index.js
@@ -30,7 +30,6 @@ module.exports = yeoman.Base.extend({
 		app: function() {
 			this.template('package.json', 'package.json', this);
 			this.template('electric.config.js', 'electric.config.js', this);
-			this.template('gulpfile.js', 'gulpfile.js', this);
 			this.template('README.md', 'README.md', this);
 			this.fs.copy(
 				this.templatePath('gitignore'),
